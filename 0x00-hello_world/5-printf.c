2964ac73d3893fd75d088623beedade65396fb90
sweKibagendi <swekibagendi@gmail.com>
2022-10-16 10:52:47 -0700

    Printf function: prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char myString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", myString);
+	return (0);
+}
