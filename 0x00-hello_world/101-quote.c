ace819be5656233798edf875e843940c26b4c5d4
sweKibagendi <swekibagendi@gmail.com>
2022-10-19 07:53:24 -0700

    Program: prints to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Description: prints to standard error
+ * Return: 1
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+return (1);
+}
