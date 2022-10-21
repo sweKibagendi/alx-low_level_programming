7b2aa8e8151724dcbb24861ddecab0b5dea80937
sweKibagendi <swekibagendi@gmail.com>
2022-10-21 05:23:56 -0700

    Defined mul function: multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies to integers
+ * @a: arg1
+ * @b: arg2
+ * Return: product of a and b
+ *
+ * Pseudocode
+ * START
+ * Return a * b
+ * END
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
