cd84ee0a5775bc9738c515a67747825cb9992233
sweKibagendi <swekibagendi@gmail.com>
2022-10-25 10:42:20 -0700

    Defined _strlen function: returns the lengthe of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - gets the length of a string
+ * @s: pointer to string
+ * return: length of the string
+ */
+int _strlen(char *s)
+{
+	auto int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	i += 1;
+
+	return i;
+}
