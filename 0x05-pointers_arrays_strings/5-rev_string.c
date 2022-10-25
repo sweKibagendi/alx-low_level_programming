e8013d475c6e61454553780fb2db90710c09803f
sweKibagendi <swekibagendi@gmail.com>
2022-10-25 13:48:28 -0700

    Defined rev_string function prototype

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: pointer to string
+ */
+void rev_string(char *s)
+{
+	auto int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(*s);
+		s--;
+		i--;
+	}
+	_putchar('\n');
+}
