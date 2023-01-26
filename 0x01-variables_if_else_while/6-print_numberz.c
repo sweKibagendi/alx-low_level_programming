51ec2892b72b121718c173cf83545daba18c6924
Wesley Kibagendi <wesoon98@gmail.com>
2023-01-26 22:32:39 +0300

    Program: prints base 10 digits with ascii rep

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints base 10 digits
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/**
+	 * Declare int n
+	 * For loop
+	 * Initialize n to 48
+	 * Set limit to 57
+	 * Increment n
+	 * Print n
+	 * Print newline
+	 */
+
+	int n;
+
+	for (n = 48; n <= 57; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
