8a60c2f6ff7fcb1d4384b564e7b5de1cc55ad194
Wesley Kibagendi <wesoon98@gmail.com>
2023-01-26 18:26:39 +0300

    Program: prints last digit of n to eaither greater 5, zero or less 6 and not 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints last digit on n
+ * Return: Always 0
+ **/
+int main(void)
+{
+	int n;
+	int num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	num = n % 10;
+	if (num > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
+	}
+	else if (num == n)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, num);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+	}
+	return (0);
+}
